ac9c23882c5dd0972112cc83576bfc834174bb0c
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-06-29 17:33:24 +0200

    Construction code pour la page Single Character

--- js/index-script.js
+++ js/index-script.js
@@ -1,26 +1,50 @@
-// const axios = require('axios');
+let template;
+let templateClone;
+let newPlace;
+let charactersDatabase;
+let focus = document.hasFocus();
 
-async function getCharactersFromDatabase()
-{
-	let charactersDatabase = await fetch("https://character-database.becode.xyz/characters");
-	let data = await charactersDatabase.json();
-	return data;
-}
 
-getCharactersFromDatabase()
+charactersDatabase = fetch("https://character-database.becode.xyz/characters")
 .then((data) =>
 {
-	let template = document.getElementById("template").content;
-	let newPlace = document.getElementById("target");
-	
-	data.forEach((element) =>
-	{
-		let templateClone = template.cloneNode(true);
-		
-		templateClone.getElementById("image").src = "data:image/gif;base64," + element.image;
-		templateClone.getElementById("name").innerHTML = element.name;
-		templateClone.getElementById("short-description").innerHTML = element.shortDescription;
-
-		newPlace.appendChild(templateClone);
-	})
+	return data.json();
 })
+.then((data) =>
+{
+	template = document.getElementById("template").content;
+	newPlace = document.getElementById("target");
+	templateClone = template.cloneNode(true);
+	
+	// if (focus === "index.html")
+	// {
+		data.forEach((element) =>
+		{
+			templateClone = template.cloneNode(true);
+		
+			templateClone.querySelector(".image").src = "data:image/gif;base64," + element.image;
+			templateClone.querySelector(".card__title").innerHTML = element.name;
+			templateClone.querySelector(".card__id").innerHTML = element.id;
+			templateClone.querySelector(".card__short-description").innerHTML = element.shortDescription;
+			templateClone.querySelector(".card__description").innerHTML = element.description;
+
+			newPlace.appendChild(templateClone);
+		})
+	// }
+	// else
+	// {
+		let viewButtons = document.getElementsByClassName("view");
+		console.log(viewButtons);
+
+		for (i = 0; i < viewButtons.length; i++)
+		{
+			viewButtons[i].addEventListener("click", () =>
+			{
+				if (viewButtons[i] === data)
+				{
+					
+				}
+			})
+		}
+	// }
+});
