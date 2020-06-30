2c9834a81bbe8101b95bab1dca7c0af87ff52e32
Caroline <carolineverpoorten@gmail.com>
2020-06-30 12:04:09 +0200

    images work and style

--- js/create.js
+++ js/create.js
@@ -1,24 +1,40 @@
+let reader;
+let result;
+let img;
+let charName;
+let shortDesc;
+let charDesc;
 
-
+function encodeImageFileAsURL(element){
+    let file = element.files[0];
+    reader= new FileReader();
+    reader.onloadend = function() {
+        result = reader.result;
+        img = result.substring(23, result.length)
+        console.log("RESULT", img)
+    }
+    reader.readAsDataURL(file);
+}
 
 document.getElementById("submit").addEventListener("click", function() {
-    
-    let charName = document.getElementById("name").value;
-    let shortDesc = document.getElementById("short-description").value;
-    let charDesc = document.getElementById("description").value;
+
+    charName = document.getElementById("name").value;
+    shortDesc = document.getElementById("short-description").value;
+    charDesc = document.getElementById("description").value;
     
     fetch("https://character-database.becode.xyz/characters", {
 
         method: "POST",
         headers: {
             Accept: "application/json",                
-            "Content-Type": "application/json",
+            "Content-Type": "application/json; charset=utf-8",
         },
 
         body: JSON.stringify({
             name: charName,
             description: charDesc,
             shortDescription: shortDesc,
+            image: img,
         }),
     });
 
