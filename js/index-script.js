00aca2091b9680f816abf7170c6d11ed537209c3
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-07-01 11:21:49 +0200

    Ajout fonction pour changer le titre de la page

--- js/index-script.js
+++ js/index-script.js
@@ -4,9 +4,7 @@ let templateClone;
 let templateClone2;
 let newPlace1;
 let newPlace2;
-let charactersDatabase;
-let selectedData;
-let viewButtons;
+let mainTitle;
 
 
 charactersDatabase = fetch("https://character-database.becode.xyz/characters")
@@ -40,23 +38,43 @@ charactersDatabase = fetch("https://character-database.becode.xyz/characters")
 	{
 		el.addEventListener("click", () =>
 		{
+			changeMainTitle();
+			
 			clicid = el.getAttribute("id");
 			let select = data.find(element => element.id == clicid);
 
+			// Masquer l'affichage des cartes
 			document.getElementById("target").style.display = "none";
-			
-			templateClone2.querySelector(".image").src = "data:image/gif;base64," + select.image;
-			templateClone2.querySelector(".card__title").innerHTML = select.name;
-			
-			templateClone2.querySelector(".card__id").style.display = "block";
-			templateClone2.querySelector(".card__id").innerHTML = select.id;
 
-			templateClone2.querySelector(".card__short-description").innerHTML = select.shortDescription;
+			document.getElementById("target2").style.display = "block";
+			// document.getElementById("target2").style.width = "400px";
 			
-			templateClone2.querySelector(".card__description").style.display = "block";
-			templateClone2.querySelector(".card__description").innerHTML = select.description;
+			// Placement des valeurs dans la copie du template
+			templateClone2.querySelector(".image2").src = "data:image/gif;base64," + select.image;
+			templateClone2.querySelector(".card__title2").innerHTML = select.name;
+			
+			// Afficher l'id et attribuer sa valeur
+			templateClone2.querySelector(".card__id2").style.display = "block";
+			templateClone2.querySelector(".card__id2").innerHTML = select.id;
+
+			templateClone2.querySelector(".card__short-description2").innerHTML = select.shortDescription;
+			
+			// Afficher la description et attribuer sa valeur
+			templateClone2.querySelector(".card__description2").style.display = "block";
+			templateClone2.querySelector(".card__description2").innerHTML = select.description;
 
 			newPlace2.appendChild(templateClone2);
 		});
-	})
+
+		document.getElementById("reload").addEventListener("click", () =>
+		{
+			location.reload();
+		})
+	});
 });
+
+function changeMainTitle()
+{
+	mainTitle = document.querySelector("#title");
+	mainTitle.innerHTML = "SINGLE CHARACTER";
+}
