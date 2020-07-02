1e5d7610552527c1ebe2319330bafd08681ff0fc
Arnaud-De-Baerdemaeker <de.baerdemaeker.arnaud@gmail.com>
2020-07-02 14:21:19 +0200

    Merge en un code

--- js/index-script.js
+++ js/index-script.js
@@ -1,10 +1,46 @@
 let template1;
 let template2;
+
 let templateClone;
 let templateClone2;
+
 let newPlace1;
 let newPlace2;
+
 let mainTitle;
+
+let template3;
+let template4;
+
+let templateClone3;
+let templateClone4;
+
+let newPlace3;
+let newPlace4;
+
+let charactersDatabase;
+
+let selectedData;
+
+let viewButtons;
+
+let reader;
+let result;
+let img;
+let charName;
+let shortDesc;
+let charDesc;
+
+function encodeImageFileAsURL(element){
+    let file = element.files[0];
+    reader= new FileReader();
+    reader.onloadend = function() {
+        result = reader.result;
+        img = result.substring(23, result.length)
+        console.log("RESULT", img)
+    }
+    reader.readAsDataURL(file);
+}
 
 
 // Récupération des données de l'API
@@ -19,9 +55,13 @@ charactersDatabase = fetch("https://character-database.becode.xyz/characters")
 	template1 = document.getElementById("template-list").content;
 	// Récupération des éléments du template 2
 	template2 = document.getElementById("template-character").content;
+	template3 = document.getElementById("template-editor").content;
+	template4 = document.getElementById("template-delete").content;
 	// Stocker la cible dans des variables
 	newPlace1 = document.getElementById("target");
 	newPlace2 = document.getElementById("target2");
+	newPlace3 = document.getElementById("target3");
+	newPlace4 = document.getElementById("target4");
 	
 	// Pour chaque élément de data...
 	data.forEach((element) =>
@@ -29,12 +69,20 @@ charactersDatabase = fetch("https://character-database.becode.xyz/characters")
 		// Cloner les templates
 		templateClone = template1.cloneNode(true);
 		templateClone2 = template2.cloneNode(true);
+		templateClone3 = template3.cloneNode(true);
+		templateClone4 = template4.cloneNode(true);
 		
 		// Inscrire les données concernées de l'API dans la copie du template
 		templateClone.querySelector(".image").src = "data:image/gif;base64," + element.image;
+		if (element.image === "")
+		{
+			templateClone.querySelector(".card__image").style.display = "none";
+		}
 		templateClone.querySelector(".card__title").innerHTML = element.name;
 		templateClone.querySelector(".card__short-description").innerHTML = element.shortDescription;
 		templateClone.querySelector(".view").setAttribute("id", element.id);
+		templateClone.querySelector(".edit").setAttribute("id", element.id);
+		templateClone.querySelector(".delete").setAttribute("id", element.id);
 		
 		// Placer le clone du template rempli avec les données dans la cible 1
 		newPlace1.appendChild(templateClone);
@@ -58,14 +106,17 @@ charactersDatabase = fetch("https://character-database.becode.xyz/characters")
 			let select = data.find(element => element.id == clicid);
 
 			// Masquer l'affichage des cartes
-			// document.getElementById("target").style.display = "none";
-			swipeDown();
+			document.getElementById("target").style.display = "none";
 			// Afficher uniquement la carte du personnage sélectionné
 			document.getElementById("target2").style.display = "block";
 			
 			// Placer les valeurs dans la copie du template 2
 			// Placer la valeur de l'image
 			templateClone2.querySelector(".image2").src = "data:image/gif;base64," + select.image;
+			if (select.image === "")
+			{
+				templateClone2.querySelector(".card__image2").style.display = "none";
+			}
 			// Placer la valeur de name
 			templateClone2.querySelector(".card__title2").innerHTML = select.name;
 			// Afficher l'id
@@ -89,17 +140,100 @@ charactersDatabase = fetch("https://character-database.becode.xyz/characters")
 			location.reload();
 		})
 	});
+
+
+	let clickEdit;
+    
+    document.querySelectorAll(".edit").forEach((element) =>
+	{
+        element.addEventListener("click", () =>
+		{
+            clickEdit= element.getAttribute("id");
+			let change = data.find(element => element.id == clickEdit);
+
+            document.getElementById("target").style.display = "none";
+			
+            templateClone3.querySelector(".image").src = "data:image/gif;base64," + change.image;
+            
+			templateClone3.querySelector(".edit__title").innerHTML = change.name;
+
+			templateClone3.querySelector(".edit__short-description").innerHTML = change.shortDescription;
+
+            templateClone3.querySelector(".edit__description").style.display = "block";
+			templateClone3.querySelector(".edit__description").innerHTML = change.description;
+
+            newPlace3.appendChild(templateClone3);
+            
+            document.getElementById("changes").addEventListener("click", function() {
+
+                charName = document.querySelector(".edit__title").value;
+                shortDesc = document.querySelector(".edit__short-description").value;
+                charDesc = document.querySelector(".edit__description").value;
+                
+                fetch("https://character-database.becode.xyz/characters/" + clickEdit, {
+            
+                    method: "PUT",
+                    headers: {
+                        Accept: "application/json",                
+                        "Content-Type": "application/json; charset=utf-8",
+                    },
+            
+                    body: JSON.stringify({
+                        id: clickEdit,
+                        name: charName,
+                        description: charDesc,
+                        shortDescription: shortDesc,
+                        image: img || change.image,
+                    }),
+                });
+
+                setTimeout(function(){
+                    location.reload();
+                },1000);
+            });
+        })
+	});
+	
+
+	let clickDelete;
+    
+    document.querySelectorAll(".delete").forEach((element) =>
+	{
+        element.addEventListener("click", () =>
+		{
+            clickDelete = element.getAttribute("id");
+			let toBeDeleted = data.find(element => element.id == clickDelete);
+
+            document.getElementById("target").style.display = "none";
+			
+            templateClone4.querySelector(".image").src = "data:image/gif;base64," + toBeDeleted.image;
+            
+			templateClone4.querySelector(".card__title").innerHTML = toBeDeleted.name;
+
+            newPlace4.appendChild(templateClone4);
+            
+            document.getElementById("approve-delete").addEventListener("click", function() {
+                
+                fetch("https://character-database.becode.xyz/characters/" + clickDelete, {
+            
+                    method: "DELETE",
+                
+                });
+
+                setTimeout(function(){
+                    location.reload();
+                },1000);
+            });
+
+            document.getElementById("refuse-delete").addEventListener("click", function() {
+                location.reload();
+            })
+        })
+    });
 });
 
 function changeMainTitle()
 {
 	mainTitle = document.querySelector("#title");
 	mainTitle.innerHTML = "SINGLE CHARACTER";
-}
-
-function swipeDown()
-{
-	let targetClass = document.querySelector(".target");
-	targetClass.classList.add("targetDown");
-	targetClass.classList.remove("target");
 }
